0d36520d566d066f627dc256064826ae11d2749a
Astm <astm_desig@hotmail.com>
2019-05-29 09:57:39 +0200

    projects scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_071601) do
+ActiveRecord::Schema.define(version: 2019_05_29_075516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "details"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "projects/edit", type: :view do
+  before(:each) do
+    @project = assign(:project, Project.create!(
+      :name => "MyString",
+      :details => "MyText",
+      :status => 1
+    ))
+  end
+
+  it "renders the edit project form" do
+    render
+
+    assert_select "form[action=?][method=?]", project_path(@project), "post" do
+
+      assert_select "input[name=?]", "project[name]"
+
+      assert_select "textarea[name=?]", "project[details]"
+
+      assert_select "input[name=?]", "project[status]"
+    end
+  end
+end
