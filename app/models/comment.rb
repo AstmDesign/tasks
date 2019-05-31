8f0dd49264a869094e03abfb9e8c28524d7e4a3d
Astm <astm_desig@hotmail.com>
2019-05-31 22:43:11 +0200

    missing relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   # DB Relations
   has_many :comment_files, dependent: :destroy
   belongs_to :user
+  belongs_to :task
 
   # Validation
   validates :comment, presence: true
