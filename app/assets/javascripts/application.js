d8ba14f08d165363146eb3c0031106a7cd7c21d5
Astm <astm_desig@hotmail.com>
2019-06-01 17:33:07 +0200

    frontend validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery2
 //= require rails-ujs
+//= require js/bootstrap.min.js
 //= require activestorage
 //= require turbolinks
 //= require_tree .
